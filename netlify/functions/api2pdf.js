9686c5a0897756c55e01b47c66dca34007bc0a1d
Alan Sparkes <info@joomkit.com>
2021-05-06 14:32:12 +0100

    env lambda func test

--- netlify/functions/api2pdf.js
+++ netlify/functions/api2pdf.js
@@ -1,6 +1,6 @@
 const { API2PDF_API_KEY } = process.env;
 
-exports.handler = async (event, context) => {
+exports.handler = async function(event, context) {
     return {
         statusCode: 200,
         body: API2PDF_API_KEY,
