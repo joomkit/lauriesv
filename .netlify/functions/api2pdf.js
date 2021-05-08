f45d41779459810e9e3a71b2c6791cf8268670e4
Alan Sparkes <info@joomkit.com>
2021-05-08 14:28:42 +0100

    Update api2pdf.js

--- .netlify/functions/api2pdf.js
+++ .netlify/functions/api2pdf.js
@@ -1,26 +1,47 @@
 
-const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts"
+
+const html = "<p>hello john</p>"
+
+function printHtmlToPdfXHR(html) {
+    var pdfName = 'Online Psychotherapy Contract for john';
+    var endpoint = 'https://v2018.api2pdf.com/wkhtmltopdf/html';
+    var cookie = '234f8d16-47c7-469a-a9cc-f40d1c445fd9'; //replace this with your own from portal.api2pdf.com
+    var payload = {
+        "html": "<p>hello john</p>",
+        "inlinePdf": false,
+        "fileName": pdfName,
+        "options": {
+            "marginBottom": 1.3
+        }
+    };
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", endpoint, true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    /* Authorization header */
+    xhr.setRequestHeader("Authorization", cookie);
+
+    xhr.send(JSON.stringify(payload));
+
+    xhr.onreadystatechange = function () { // Call a function when the state changes.
+        if (xhr.readyState === 4 && this.status === 200) {
+            // Request finished. Do processing here.
+            var xhr_response = xhr.response;
+            console.log(xhr.response.pdf);
+            return xhr_response;
+        }
+    }
+}
 
 exports.handler = async (event, context) => {
 
-    try {
-        response = await fetch(API_ENDPOINT)
-        // handle response
-    } catch (err) {
-        return {
-            statusCode: err.statusCode || 500,
-            body: JSON.stringify({
-                error: err.message
-            })
-        }
-    }
-
+    var pdf = printHtmlToPdfXHR()
     return {
         statusCode: 200,
         body: JSON.stringify({
-            data: response
+            data: pdf
         })
     }
 }
