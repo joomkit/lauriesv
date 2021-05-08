3621e74609af687d38cbefca2c5a150db7860c05
Alan Sparkes <info@joomkit.com>
2021-05-08 23:03:09 +0100

    Update booking.js

--- public/booking.js
+++ public/booking.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     async function api2pdf(html,cName) {
         console.log(cName.value);
         console.log(html);
-        var data = { html: html, name: cName.value };
+        var mydata = { html: html, name: cName.value };
         const url = '/.netlify/functions/api2pdf';
         try {
             const response = await fetch(url,{
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
                     'Content-Type': 'application/json'
                 },
                 //body: JSON.stringify(user)
-                body: JSON.stringify(data)
+                body: JSON.stringify(mydata)
             });
             const data = await response.json();
             console.log(data);
