2dfb118907b960caaad2dfdba3843a42018e8882
Alan Sparkes <info@joomkit.com>
2021-05-09 15:19:03 +0100

    Update api2pdf.js

--- .netlify/functions/api2pdf.js
+++ .netlify/functions/api2pdf.js
@@ -55,7 +55,7 @@ const fetch = require("node-fetch");
 const API_ENDPOINT = "https://v2018.api2pdf.com/wkhtmltopdf/html";
 
 exports.handler = async (event, context) => {
-    const html = JSON.parse(event.body.html)
+    const html = event.body.html
 
     console.log(event.body);
     return (html)
