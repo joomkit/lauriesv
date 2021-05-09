f5afdb9a9fa3f7d6f54f60dcf67b3087a2f2971c
Alan Sparkes <info@joomkit.com>
2021-05-09 19:56:06 +0100

    Update api2pdf.js

--- .netlify/functions/api2pdf.js
+++ .netlify/functions/api2pdf.js
@@ -13,7 +13,7 @@ console.log("hello");
 
     const pdfName = 'Online Psychotherapy Contract';
     const payload = {
-        "html": ""+name+"",
+        "html": ""+cpdfHTMLStr+"",
         "inlinePdf": false,
         "fileName": pdfName,
         "options": {
